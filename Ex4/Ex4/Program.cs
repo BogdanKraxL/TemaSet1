455b52644db2501e7748ddad970ab3385c2ef558
Taichis Bogdan <116056308+BogdanKraxL@users.noreply.github.com>
2022-10-24 17:45:56 +0300

    Add files via upload

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Detreminati daca un an y este an bisect. 
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti un numar de ani: ");
+            int y = int.Parse(Console.ReadLine());
+            if ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0))
+            {
+                Console.WriteLine(y + " este an bisect");
+            }
+            else 
+            {
+                Console.WriteLine(y + " nu este an bisect"); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Extrageti si afisati a k-a cifra de la sfarsitul unui numar. Cifrele se numara de la dreapta la stanga. 
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, k;
+            num = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            for(int i=1; i<k; i++)
+            {
+                num /= 10;
+            }
+            Console.WriteLine(num % 10);
+
+            Console.ReadKey();
+        }    
+    }   
+}
