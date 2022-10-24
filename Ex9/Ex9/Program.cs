600e280eeeb5063b64892643ae48f43d586dfc30
Taichis Bogdan <116056308+BogdanKraxL@users.noreply.github.com>
2022-10-24 18:12:50 +0300

    Add files via upload

---
+++ Ex9/Ex9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//Afisati toti divizorii numarului n. 
+
+namespace Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Introduceti un numar: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
