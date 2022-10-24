b28f7f8490a361f2c73da590c058389b9335a212
Taichis Bogdan <116056308+BogdanKraxL@users.noreply.github.com>
2022-10-24 18:33:00 +0300

    Add files via upload

---
+++ Ex10/Ex10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//Test de primalitate: determinati daca un numar n este prim.
+
+namespace Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            bool prim = true;
+            if (n < 1)
+            {
+                prim = false;
+                Console.WriteLine(n + " nu este prim");
+            }
+            else
+            {
+                if (n == 1 || n == 2)
+                {
+                    prim = true;
+                    Console.WriteLine(n + " este prim");
+                }
+                else
+                {
+                    for (int d = 3; d < n; d++)
+                    {
+                        if (n % d == 0)
+                        {
+                            prim = false;
+                        }
+                    }
+                    if (prim == true)
+                        Console.WriteLine(n + " este prim");
+                    else
+                        Console.WriteLine(n + " nu este prim");
+
+                }
+
+            }
+        }
+    }
+}
