cd934a35a3e6d28d6ac7b057b6d41d9bd54cc758
Taichis Bogdan <116056308+BogdanKraxL@users.noreply.github.com>
2022-10-25 12:44:00 +0300

    Add files via upload

---
+++ Ex7/Ex7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//(Swap) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor.
+
+namespace Ex7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("a = {0} \\ b = {1}", b, a);
+        }
+    }
+
+}
+ 
