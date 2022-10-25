653d865372fa9b393b1afa0c212df1a0b1fc8162
Taichis Bogdan <116056308+BogdanKraxL@users.noreply.github.com>
2022-10-26 01:35:54 +0300

    Add files via upload

---
+++ Ex11/Ex11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Afisati in ordine inversa cifrele unui numar n. 
+
+namespace Ex11
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, reverse = 0, rem;
+            Console.Write("Introduceti un numar: ");
+            n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+
+            }
+            Console.WriteLine("Numarul tau invers este: {0}", reverse);
+        }
+    }
+}
