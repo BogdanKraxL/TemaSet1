7f6969b764f2b6b75d7d972add27b8f7138187c9
Taichis Bogdan <116056308+BogdanKraxL@users.noreply.github.com>
2022-11-07 06:07:12 +0200

    Add files via upload

---
+++ Ex12/Ex12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Security.AccessControl;
+using System.Text; 
+
+//Determinati cate numere intregi divizibile cu n se afla in intervalul [a, b].
+
+namespace Ex12
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, n, nr = 0;
+            Console.Write("Primul numar din interval: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Al doilea numar din interval: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Numerele divizibile cu: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Din intervalul [{a} {b}]");
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % n == 0)
+                {
+                    nr++;
+                }
+            }
+            Console.WriteLine(nr);
+        }
+    }
+}
+
---
+++ Ex13/Ex13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Determianti cati ani bisecti sunt intre anii y1 si y2.
+
+namespace Ex13
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int y1, y2;
+            Console.Write("De la anul: ");
+            y1 = int.Parse(Console.ReadLine());
+            Console.Write("Pana la anul: ");
+            y2 = int.Parse(Console.ReadLine());
+            
+            for (int n = y1; n < y2; n++)
+            {
+                if (((n % 4 == 0) && (n % 100 != 0)) || (n % 400 == 0))
+                {
+                    Console.WriteLine($"Anul {n} este an bisect");
+                }
+                else
+                {
+                    Console.WriteLine($"Anul {n} nu este an bisect");
+                }
+            }
+        }
+    }
+
+}
