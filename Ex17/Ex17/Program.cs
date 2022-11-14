2aa2a59b0a09e99e240d62b996e9bc2e08c9282b
Taichis Bogdan <116056308+BogdanKraxL@users.noreply.github.com>
2022-11-14 17:10:00 +0200

    Add files via upload

---
+++ Ex17/Ex17/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Determianti cel mai mare divizor comun si cel mai mic multiplu comun a doua numere. Folositi algoritmul lui Euclid. 
+
+namespace Ex17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            int a, b;
+
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            n = a;
+            m = b;
+
+            while (n != m)
+            {
+                if (n > m)
+                {
+                    n -= m;
+                }
+                else 
+                {
+                    m -= n;
+                }                   
+            }
+            Console.WriteLine(n);
+
+            n = a;
+            m = b;
+
+            while (m != n)
+            {
+                if (n < m)
+                {
+                    n += a;
+                }
+                else
+                {
+                    m += b;
+                }
+            }
+            Console.WriteLine(n);
+        }
+            
+    }
+}
---
+++ Ex18/Ex18/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Afisati descompunerea in factori primi ai unui numar n.  De ex. pentru n = 1176 afisati 2^3 x 3^1 x 7^2. 
+
+namespace Ex18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Introduceti un numar n = ");
+            n = int.Parse(Console.ReadLine());
+
+            int div = 2;
+            int exp = 0;
+
+            while(n != 1)
+            {
+                while (n % div == 0)
+                {
+                    exp++;
+                    n = n / div;
+                }
+                if (exp != 0)
+                {
+                    Console.Write($"{div} ^ {exp}\n");
+                    exp = 0;
+                }
+                div++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
